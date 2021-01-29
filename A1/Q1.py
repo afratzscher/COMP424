42cf4a0ddff46b05f4617f3b4dc8ce3b38aa0517
Anne-Sophie <anne-sophie.fratzscher@mail.mcgill.ca>
2021-01-28 20:58:32 -0500

    uniform cost search done

--- A1/Q1.py
+++ A1/Q1.py
@@ -3,22 +3,23 @@ import collections
 import math
 
 class state:
-	def __init__(self,cost,depth,parent,positions,move):
+	def __init__(self,cost,depth,parent,positions,move,direction):
 		self.depth = depth
 		self.cost = cost
 		self.parent = parent
 		self.positions = positions
 		self.move = move
+		self.direction=direction
 
 def get_index(positions):
 	row,col = [*np.where(positions == 0)]
 	return row[0], col[0]
 
-def check_visited(direction, curr_state, positions, visited):
+def check_visited(direction, curr_state, positions, visited, val):
 	for i in visited:
 		if (positions == i).all():
 			return None
-	return state(curr_state.cost+1, curr_state.depth+1, curr_state, positions, direction)
+	return state(curr_state.cost+1, curr_state.depth+1, curr_state, positions, val, direction)
 	
 def move(curr_state, direction, visited):
 	positions = curr_state.positions.copy()
@@ -26,39 +27,39 @@ def move(curr_state, direction, visited):
 	if direction is 'L':
 		try: 
 			positions[row, col], positions[row, col+1] = positions[row, col+1], positions[row, col]
-			return check_visited(direction, curr_state, positions, visited), positions[row,col]
+			return check_visited(direction, curr_state, positions, visited, positions[row,col])
 		except:
-			return None, None
+			return None
 	if direction is 'R':
 		try: 
 			if col-1 >= 0: # accounts for list[-1] allowance
 				positions[row, col], positions[row, col-1] = positions[row, col-1], positions[row, col]
-				return check_visited(direction,curr_state, positions, visited), positions[row,col]
+				return check_visited(direction,curr_state, positions, visited, positions[row,col])
 			else:
-				return None, None
+				return None
 		except:
-			return None, None
+			return None
 	if direction is 'U':
 		try: 
 			positions[row, col], positions[row+1, col] = positions[row+1, col], positions[row, col]
-			return check_visited(direction, curr_state, positions, visited), positions[row,col]
+			return check_visited(direction, curr_state, positions, visited, positions[row,col])
 		except:
-			return None, None
+			return None
 	if direction is 'D':
 		try: 
 			if row-1 >= 0: # accounts for list[-1] allowance
 				positions[row, col], positions[row-1, col] = positions[row-1, col], positions[row, col]
-				return check_visited(direction, curr_state, positions, visited), positions[row,col]
+				return check_visited(direction, curr_state, positions, visited, positions[row,col])
 			else:
-				return None, None
+				return None
 		except:
-			return None, None
+			return None
 
 def backtrack(goal_state, start_state):
 	solution = []
 	curr_state = goal_state 
 	while not ((curr_state.positions == start_state.positions).all()):
-		solution.insert(0,curr_state.move)
+		solution.insert(0, curr_state.direction)
 		# print(curr_state.positions)
 		curr_state = curr_state.parent
 	# print(start_state.positions)
@@ -77,13 +78,44 @@ def bfs(start_state):
 		curr_state = queue.pop(0)
 		toadd = {}
 		for i in ['L', 'R', 'U', 'D']:
-			temp, moved_val = move(curr_state, i, visited)
+			temp = move(curr_state, i, visited)
 			if temp:
-				toadd[moved_val] = temp
+				toadd[temp.move] = temp
 		ordered_add = collections.OrderedDict(sorted(toadd.items()))
 		for key in ordered_add:
 			visited.append(ordered_add[key].positions)
 			queue.append(ordered_add[key])
+	
+	return curr_state.cost, backtrack(curr_state, start_state)
+
+'''RUNS UNIFORM COST SEARCH'''
+def uniform(start_state):
+	queue = {}
+	visited = []
+	queue[0] = [start_state]
+	visited.append(start_state.positions)
+	curr_state = start_state
+
+	k = 0
+	while not (curr_state.positions == goal_state).all():
+		curr_state = queue[min(queue.keys())].pop(0)
+		if (len(queue[min(queue.keys())]) == 0):
+			queue.pop(min(queue.keys()))
+		toadd = {}
+		for i in ['L', 'R', 'U', 'D']:
+			temp = move(curr_state, i, visited)
+			if temp:
+				toadd[temp.move] = temp
+		ordered_add = collections.OrderedDict(sorted(toadd.items()))
+		for key in ordered_add:
+			visited.append(ordered_add[key].positions)
+			try:
+				currlist = queue[ordered_add[key].cost]
+			except:
+				currlist = []
+			currlist.append(ordered_add[key])
+			currlist.sort(key=lambda x: x.move, reverse=False)
+			queue[ordered_add[key].cost] = currlist
 	
 	return curr_state.cost, backtrack(curr_state, start_state)
 
@@ -101,9 +133,9 @@ def dfs(start_state, *args):
 	while (not (curr_state.positions == goal_state).all()):
 		toadd = {}
 		for i in ['L', 'R', 'U', 'D']:
-			temp, moved_val = move(curr_state, i, visited)
+			temp = move(curr_state, i, visited)
 			if temp:
-				toadd[moved_val] = temp
+				toadd[temp.move] = temp
 		try: 
 			minstate = min(toadd.keys())
 			curr_state = toadd[minstate]
@@ -139,9 +171,11 @@ def ids(start_state, max_depth):
 def main():
 	global goal_state
 	goal_state = [[0,1,2],[5,4,3]]
-	start_state = state(0, 0, None, np.array(([[1,4,2],[5,3,0]])), None)
+	start_state = state(0, 0, None, np.array(([[1,4,2],[5,3,0]])), None, None)
 	cost, solution = bfs(start_state)
 	print('----BFS----\nCOST: ', cost, '\nSOLUTION: ', solution, '\n')
+	cost, solution = uniform(start_state)
+	print('----UNIFORM----\nCOST: ', cost, '\nSOLUTION: ', solution, '\n')
 	cost, solution = dfs(start_state)
 	print('----DFS----\nCOST: ', cost, '\nSOLUTION: ', solution, '\n')
 	cost, solution = ids(start_state, 3)
